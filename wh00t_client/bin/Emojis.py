d8bb41b96b4e3b4088372888bd995976c38f2b99
roboto84 <7658704+roboto84@users.noreply.github.com>
2020-12-15 12:04:26 -0500

    feat(emoji): emojis supported by page up/down
    Dictionary that defines which emojis are treated as first class
    citizens by including them in the page up and page down quick
    emoji selectiong functionality.  Chat client has support for most
    emojis given the correct emoji code, ex:':santa:' will present
    the 🎅 emoji.

---
+++ wh00t_client/bin/Emojis.py
@@ -0,0 +1,29 @@
+# Chat Client Emojis Supported by Paging
+
+Emojis = {
+    ':smile_cat:': '😸',
+    ':thumbsup:': '👍',
+    ':thumbsdown:': '👎',
+    ':clap:': '👏',
+    ':poop:': '💩',
+    ':rolling_on_the_floor_laughing:': '🤣',
+    ':joy:': '😂',
+    ':smiling_face_with_smiling_eyes:': '😊',
+    ':slightly_smiling_face:': '🙂',
+    ':yum:': '😋',
+    ':thinking_face:': '🤔',
+    ':neutral_face:': '😐',
+    ':face_with_rolling_eyes:': '🙄',
+    ':face_with_raised_eyebrow:': '🤨',
+    ':person_facepalming:': '🤦',
+    ':exploding_head:': '🤯',
+    ':tired_face:': '😫',
+    ':sob:': '😭',
+    ':face_with_head_bandage:': '🤕',
+    ':bow:': '🙇',
+    ':pray': '🙏',
+    ':brain:': '🧠',
+    ':snake:': '🐍',
+    ':eyes:': '👀',
+    ':fire:': '🔥'
+}
