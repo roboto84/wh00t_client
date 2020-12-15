04df9a14e4343dc44d761c2cf7f75947cc36e74f
roboto84 <7658704+roboto84@users.noreply.github.com>
2020-12-15 12:22:26 -0500

    feat(helpMenu): general chat and meme help dialogs

---
+++ wh00t_client/bin/Help.py
@@ -0,0 +1,23 @@
+# Chat Client Help Menus Text
+
+HelpMenu = [
+    '/help - prints this help summary',
+    '/sound - turn notification sounds on',
+    '/noSound - turn notification sounds off',
+    '/notify - turn notification toast on',
+    '/noNotification - turn notification toast off',
+    '/emojis - print list of page up/down supported emojis',
+    '/meme {memeName} - sends specific meme as a message. See /memes',
+    '/memes - prints out a list of available meme commands'
+]
+
+HelpMeme = [
+    '/meme luv - prints luv bear meme',
+    '/meme wise - prints ascii owl',
+    '/meme tableFlip - prints ascii table flip',
+    '/meme hurry - prints hurry flying ascii',
+    '/meme bye - prints ascii bye dude',
+    '/meme hug - prints ascii hug dude',
+    '/meme shrug - prints ascii shrug',
+    '/meme kitty - prints ascii kitty whiskers'
+]
