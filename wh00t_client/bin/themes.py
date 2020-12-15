350d64d94da5e17de2e92c230c1a3bed851a470d
roboto84 <7658704+roboto84@users.noreply.github.com>
2020-12-15 12:55:20 -0500

    feat(themes): definition of client color themes

---
+++ wh00t_client/bin/themes.py
@@ -0,0 +1,47 @@
+# Chat Client theme dictionaries
+
+BaseThemeBackup = {
+    'background_color': '#292929',
+    'border_color': '#944A00',
+    'font_color': '#FF6100',
+    'mouse_over_color': '#D66C00',
+    'highlight_background_color': '#292929',
+    'highlight_color': '#292929',
+    'insert_background': '#FF6100',
+    'active_background': '#D66C00',
+    'specialSelectorBackground': '#EEE000'
+}
+
+BaseTheme = {
+    'app_background_color': '#292929',
+    'background_color': '#1d1f21',
+    'border_color': '#944A00',
+    'button_color': '#de935f',
+    'entry_field_color': '#de935f',
+    'font_color': '#f0c674',
+    'mouse_over_color': '#D66C00',
+    'highlight_background_color': '#944A00',
+    'highlight_color': '#944A00',
+    'insert_background': '#FF6100',
+    'active_background': '#D66C00',
+    'emoji_color': '#b5bd68',
+    'bracket_highlight_color': '#8abeb7',
+    'user_handle_color': '#b5bd68',
+    'other_user_handles_color': '#de935f',
+    'system_color': '#81a2be'
+}
+
+TomorrowNight = {
+    'Background': '#1d1f21',
+    'CurrentLine': '#282a2e',
+    'Selection': '#373b41',
+    'Foreground': '#c5c8c6',
+    'Comment': '#969896',
+    'Red': '#cc6666',
+    'Orange': '#de935f',
+    'Yellow': '#f0c674',
+    'Green': '#b5bd68',
+    'Aqua': '#8abeb7',
+    'Blue': '#81a2be',
+    'Purple': '#b294bb'
+}
