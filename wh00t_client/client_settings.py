a838b703be1a9e90939701f8af98f20db792ee69
roboto84 <7658704+roboto84@users.noreply.github.com>
2020-12-17 10:48:21 -0500

    feat(client): runnable multi platform client
    Add parent class and supporting settings class which
    makes the wh00t client a runnable GUI application.
    Built in support for cross platform (linux and windows)
    configuration.  Built in support for basic GUI color themes.

---
+++ wh00t_client/client_settings.py
@@ -0,0 +1,121 @@
+# Chat Client base class
+
+import ntpath
+import os
+import time
+import platform
+from datetime import datetime
+from bin.themes import BaseTheme
+from __init__ import __version__
+
+
+class ClientSettings:
+    CLIENT_VERSION = __version__
+    CURRENT_PLATFORM = platform.system()
+    APP_TITLE = 'wh00t'
+    HOME_PATH = ntpath.dirname(__file__)
+    APP_BANNER = os.path.join(HOME_PATH, 'assets/visual/banner.png')  # Banner Should be 663w x 90h pixels
+    APP_ICON = os.path.join(HOME_PATH, 'assets/visual/icon.png')
+    MESSAGE_SOUND = os.path.join(HOME_PATH, 'assets/audio/wh00t7.wav')
+    USER_ALERT_SOUND = os.path.join(HOME_PATH, 'assets/audio/AORiver.wav')
+    ALERT_COMMAND = '/ao'
+    EXIT_STRING = '/exit'
+
+    # Chat Socket
+    HOST = '192.168.0.102'
+    PORT = 3001
+    BUFFER_SIZE = 1024
+    ADDR = (HOST, PORT)
+
+    def __init__(self):
+        self.sound_alert_preference = True
+        self.notification_alert_preference = True
+        self.app_background_color = BaseTheme['app_background_color']
+        self.background_color = BaseTheme['background_color']
+        self.border_color = BaseTheme['border_color']
+        self.button_color = BaseTheme['button_color']
+        self.entry_field_color = BaseTheme['entry_field_color']
+        self.font_color = BaseTheme['font_color']
+        self.mouse_over_color = BaseTheme['mouse_over_color']
+        self.highlight_background_color = BaseTheme['highlight_background_color']
+        self.highlight_color = BaseTheme['highlight_color']
+        self.insert_background = BaseTheme['insert_background']
+        self.active_background = BaseTheme['active_background']
+        self.emoji_color = BaseTheme['emoji_color']
+        self.bracket_highlight_color = BaseTheme['bracket_highlight_color']
+        self.user_handle_color = BaseTheme['user_handle_color']
+        self.other_user_handles_color = BaseTheme['other_user_handles_color']
+        self.system_color = BaseTheme['system_color']
+
+        if self.CURRENT_PLATFORM == 'Windows':
+            # import pyglet
+            from win10toast import ToastNotifier
+            # pyglet.font.add_file('file.ttf')
+            self.windows_notification = ToastNotifier()
+            self.app_dimensions = {'width': 668, 'height': 420}
+            self.message_list_font = ('Calibri', 13)  # Possible future fail safes Veranda, Consolas
+            self.entry_field_font = ('Calibri', 17)
+            self.button_font = ('Calibri', 13)
+            self.emoji_font_size = 32
+            self.bracket_highlight_font_size = 16
+            self.message_list_width = 70
+            self.message_input_width = 45
+            self.message_list_highlight_thickness = 8
+            self.message_list_spacing1 = 1
+            self.message_list_pad_x = 1
+            self.message_list_pad_y = 1
+            self.message_entry_border_dimension = 1
+            self.message_submit_button_height = 1
+            self.message_submit_button_pad_y = 4
+            self.message_list_border_dimension = 2
+            self.message_submit_button_relief = 'ridge'
+
+        elif self.CURRENT_PLATFORM == 'Linux':
+            import gi
+            gi.require_version('Notify', '0.7')
+            from gi.repository import Notify
+            self.linux_notify = Notify
+            self.linux_notify.init(self.APP_TITLE)
+            self.linux_notification = self.linux_notify.Notification.new("messageAlert")
+            self.app_dimensions = {'width': 665, 'height': 435}
+            self.message_list_font = ('DejaVu Sans', 12)
+            self.entry_field_font = ('DejaVu Sans', 15)
+            self.button_font = ('DejaVu Sans', 12)
+            self.emoji_font_size = 30
+            self.bracket_highlight_font_size = 15
+            self.message_list_width = 63
+            self.message_input_width = 40
+            self.message_list_highlight_thickness = 6
+            self.message_list_spacing1 = 5
+            self.message_list_pad_x = 5
+            self.message_list_pad_y = 7
+            self.message_entry_border_dimension = 0
+            self.message_submit_button_height = 1
+            self.message_submit_button_pad_y = 5
+            self.message_list_border_dimension = 0
+            self.message_submit_button_relief = 'flat'
+
+    def set_sound_alert_preference(self, state):
+        self.sound_alert_preference = state
+
+    def set_notification_alert_preference(self, state):
+        self.notification_alert_preference = state
+
+    def get_notification_alert_preference(self):
+        return self.notification_alert_preference
+
+    def get_sound_alert_preference(self):
+        return self.sound_alert_preference
+
+    def get_current_platform(self):
+        return self.CURRENT_PLATFORM
+
+    def get_linux_notifier(self):
+        return self.linux_notification
+
+    def get_windows_notifier(self):
+        return self.windows_notification
+
+    @staticmethod
+    def message_time():
+        return datetime.fromtimestamp(time.time()).strftime('%m/%d %H:%M')
---
+++ wh00t_client/wh00t_client.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# wh00t GUI chat client.
+
+import tkinter as tk
+import tkinter.font
+import os
+from PIL import ImageTk, Image
+from client_settings import ClientSettings
+from client_network import ClientNetwork
+from client_handlers import ClientHandlers
+from meme_collection import MemeCollection
+
+
+class Wh00tClient(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.wh00t_client_settings = ClientSettings()
+        self.wh00t_client_meme_collection = MemeCollection()
+
+        # Declare window elements
+        self.geometry(
+            '{}x{}'.format(self.wh00t_client_settings.app_dimensions['width'],
+                           self.wh00t_client_settings.app_dimensions['height']))
+        self.resizable(height=False, width=False)
+        self['bg'] = self.wh00t_client_settings.app_background_color
+
+        chat_message = tkinter.StringVar()
+        chat_frame = tkinter.Frame(self)
+        background_image = ImageTk.PhotoImage(Image.open(self.wh00t_client_settings.APP_BANNER))
+
+        banner = tkinter.Label(image=background_image,
+                               bg=self.wh00t_client_settings.background_color,
+                               highlightbackground=self.wh00t_client_settings.highlight_background_color,
+                               highlightcolor=self.wh00t_client_settings.highlight_color)
+
+        message_input_field = tkinter.Entry(self,
+                                            bd=self.wh00t_client_settings.message_entry_border_dimension,
+                                            font=self.wh00t_client_settings.entry_field_font,
+                                            width=self.wh00t_client_settings.message_input_width,
+                                            textvariable=chat_message, bg=self.wh00t_client_settings.background_color,
+                                            fg=self.wh00t_client_settings.entry_field_color,
+                                            highlightbackground=self.wh00t_client_settings.highlight_background_color,
+                                            highlightcolor=self.wh00t_client_settings.highlight_color,
+                                            insertbackground=self.wh00t_client_settings.insert_background)
+
+        message_list = tkinter.Text(chat_frame,
+                                    wrap='word',
+                                    font=self.wh00t_client_settings.message_list_font,
+                                    bd=self.wh00t_client_settings.message_list_border_dimension,
+                                    height=11,
+                                    width=self.wh00t_client_settings.message_list_width,
+                                    padx=self.wh00t_client_settings.message_list_pad_x,
+                                    pady=self.wh00t_client_settings.message_list_pad_y,
+                                    spacing1=self.wh00t_client_settings.message_list_spacing1,
+                                    bg=self.wh00t_client_settings.background_color,
+                                    fg=self.wh00t_client_settings.font_color,
+                                    highlightbackground=self.wh00t_client_settings.highlight_background_color,
+                                    highlightcolor=self.wh00t_client_settings.highlight_color,
+                                    highlightthickness=self.wh00t_client_settings.message_list_highlight_thickness)
+
+        scrollbar = tkinter.Scrollbar(chat_frame,
+                                      bg=self.wh00t_client_settings.background_color,
+                                      activebackground=self.wh00t_client_settings.mouse_over_color,
+                                      command=message_list.yview)
+
+        message_list.configure(yscrollcommand=scrollbar.set)
+
+        send_message_button = tkinter.Button(self,
+                                             font=self.wh00t_client_settings.button_font,
+                                             bd=1,
+                                             relief=self.wh00t_client_settings.message_submit_button_relief,
+                                             text='📨 Send',
+                                             height=self.wh00t_client_settings.message_submit_button_height,
+                                             width=8,
+                                             command=lambda: self.wh00t_client_network.send_message(self.close_app),
+                                             bg=self.wh00t_client_settings.background_color,
+                                             fg=self.wh00t_client_settings.button_color,
+                                             pady=self.wh00t_client_settings.message_submit_button_pad_y,
+                                             activebackground=self.wh00t_client_settings.active_background,
+                                             highlightbackground=self.wh00t_client_settings.highlight_background_color,
+                                             highlightcolor=self.wh00t_client_settings.highlight_color)
+
+        self.wh00t_client_handlers = ClientHandlers(self.wh00t_client_settings, self.wh00t_client_meme_collection,
+                                                    chat_message, message_input_field, message_list)
+        self.wh00t_client_network = ClientNetwork(self.wh00t_client_settings, chat_message,
+                                                  self.wh00t_client_handlers)
+
+        # Set initial element properties.
+        self.title(self.wh00t_client_settings.APP_TITLE)
+        self.protocol('WM_DELETE_WINDOW', lambda: self.on_window_close(chat_message))
+        chat_message.set('')
+        message_list.bind('<Key>', lambda e: self.wh00t_client_handlers.message_list_event_handler(e))
+        message_input_field.bind('<Key>', lambda e: self.wh00t_client_handlers.message_entry_event_handler(e))
+        message_input_field.bind('<Return>', lambda e: self.wh00t_client_network.send_message(self.close_app))
+        message_input_field.focus()
+
+        # Set elements structure
+        banner.grid(row=0, column=1, columnspan=2)
+        chat_frame.grid(row=1, column=1, columnspan=2)
+        message_list.grid(row=1, column=1)
+        scrollbar.grid(row=1, column=2, sticky='ns')
+        message_input_field.grid(row=2, column=1, ipady=4, ipadx=4)
+        send_message_button.grid(row=2, column=2, pady=10)
+
+        # Initialize and Run App
+        self.wh00t_client_network.sock_it()
+        self.wh00t_client_handlers.thread_it(self.wh00t_client_network.receive)
+        self.mainloop()
+
+    def close_app(self):
+        if self.wh00t_client_handlers.receive_thread.is_alive():
+            self.after(50, self.close_app)
+        else:
+            self.wh00t_client_handlers.close_notify()
+            self.wh00t_client_network.client_socket.close()
+            self.quit()
+
+    def on_window_close(self, chat_message):
+        if self.wh00t_client_network.client_socket_error:
+            os._exit(1)
+        else:
+            chat_message.set(self.wh00t_client_settings.EXIT_STRING)
+            self.wh00t_client_network.send_message(self.close_app)
+
+
+if __name__ == '__main__':
+    Wh00tClient()
